9a3dcaf4ac38b53096add7ce131ac6bb6cec57f4
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2026-04-16 10:34:24 +0100

    feat(discount): add create slice

---
+++ backend/LudaFit.Server/src/LudaFit.Core/Features/Discount/Create.cs
@@ -0,0 +1,137 @@
+using System.Net;
+using FluentValidation;
+using FluentValidation.Results;
+using LudaFit.Core.Features.Common.Interfaces;
+using LudaFit.Domain.Entities;
+using LudaFit.Domain.ValueObjects;
+using LudaFit.Infrastructure.SQLite;
+using LudaFit.SharedKernel.Interfaces;
+using LudaFit.SharedKernel.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DiscountEntity = LudaFit.Domain.Entities.Discount;
+
+namespace LudaFit.Core.Features.Discount;
+
+internal static class Create
+{
+    internal sealed record Request(
+        DateOnly StartDate,
+        DateOnly EndDate,
+        uint Percent
+    );
+
+    internal sealed class Validator : AbstractValidator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(discount => discount.Percent)
+                .GreaterThan(0u);
+        }
+    }
+
+    internal sealed class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/discounts", Handle)
+                .Produces(StatusCodes.Status201Created)
+                .ProducesValidationProblem()
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status409Conflict)
+                .WithTags("Discount");
+        }
+
+        private static async Task<IResult> Handle(
+            [FromBody] Request request,
+            [FromServices] IValidator<Request> validator,
+            [FromServices] Handler handler,
+            CancellationToken cancellationToken)
+        {
+            ValidationResult validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (validationResult.IsValid is false)
+            {
+                return Results.ValidationProblem(validationResult.ToDictionary());
+            }
+
+            Result createDiscountResult = await handler.HandleAsync(request, cancellationToken);
+
+            if (createDiscountResult.IsFailure)
+            {
+                ErrorDetails errorDetails = createDiscountResult.ErrorDetails!;
+                return ToFailureHttpResult(errorDetails);
+            }
+
+            return Results.Created();
+        }
+
+        private static IResult ToFailureHttpResult(ErrorDetails errorDetails)
+        {
+            return errorDetails.StatusCode switch
+            {
+                HttpStatusCode.BadRequest => Results.BadRequest(errorDetails.ErrorMessage),
+                HttpStatusCode.NotFound => Results.NotFound(errorDetails.ErrorMessage),
+                HttpStatusCode.Conflict => Results.Conflict(errorDetails.ErrorMessage),
+                _ => Results.Problem(
+                    detail: errorDetails.ErrorMessage,
+                    statusCode: (int)errorDetails.StatusCode
+                )
+            };
+        }
+    }
+
+    internal sealed class Handler(
+        LudaFitDbContext db,
+        TimeProvider timeProvider) : IScopedType
+    {
+        public async Task<Result> HandleAsync(Request request, CancellationToken cancellationToken)
+        {
+            bool hasOverlappingDiscount = await db.Discounts
+                .AsNoTracking()
+                .AnyAsync(
+                    discount => discount.DateRange.Start <= request.EndDate
+                                && discount.DateRange.End >= request.StartDate,
+                    cancellationToken
+                );
+
+            if (hasOverlappingDiscount)
+            {
+                return Result.Failure(
+                    "Знижка з таким періодом вже існує",
+                    HttpStatusCode.Conflict
+                );
+            }
+
+            DateOnly currentDate = DateOnly.FromDateTime(timeProvider.GetUtcNow().UtcDateTime);
+
+            Result<DateRange> createDateRangeResult = DateRange.Create(
+                currentDate,
+                request.StartDate,
+                request.EndDate
+            );
+
+            if (createDateRangeResult.IsFailure)
+            {
+                return createDateRangeResult;
+            }
+
+            Result<DiscountEntity> createDiscountResult = DiscountEntity.Create(
+                createDateRangeResult.Value!,
+                request.Percent
+            );
+
+            if (createDiscountResult.IsFailure)
+            {
+                return createDiscountResult;
+            }
+
+            DiscountEntity discount = createDiscountResult.Value!;
+
+            await db.Discounts.AddAsync(discount, cancellationToken);
+            await db.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
